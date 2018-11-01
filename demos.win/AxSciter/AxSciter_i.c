709a554ea9b614050c1438157115c987c16b0107
Andrew <andrew.fedoniouk@gmail.com>
2018-11-01 12:30:14 -0700

    4.2.6.0

--- demos.win/AxSciter/AxSciter_i.c
+++ demos.win/AxSciter/AxSciter_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Thu Oct 25 19:02:38 2018
+/* at Thu Nov 01 11:37:48 2018
  */
 /* Compiler settings for AxSciter.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
