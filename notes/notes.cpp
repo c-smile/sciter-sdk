f56e2c7ec4e48c75273060fe5fcb6d2db30f3496
Andrew <andrew.fedoniouk@gmail.com>
2020-03-07 12:43:17 -0800

    4.4.1.1

--- notes/notes.cpp
+++ notes/notes.cpp
@@ -3,35 +3,6 @@
 
 #include <functional>
 
-// native API 
-
-#if 0
-static sciter::value libcurl_api() {
-
-  sciter::value api_map;
-
-/*  sciter::value api_math_map;
-
-  std::function<int(int,int)> native_sum = [](int a, int b) { return a + b; };
-  std::function<int(int,int)> native_sub = [](int a, int b) { return a - b; };
-
-  api_math_map.set_item(sciter::value("sum"), sciter::vfunc( native_sum ));
-  api_math_map.set_item(sciter::value("sub"), sciter::vfunc( native_sub ));
-  */
-  
-  //api_map.set_item(sciter::value("math"), api_math_map);
-
-  /*
-    return {
-      math: {
-        sum: {native_sum},
-        sub: {native_sub},
-      }
-    }
-  */
-  return api_map;
-}
-#endif
 
 
 class frame: public sciter::window 
